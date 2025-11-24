4ef4932be8d4dd1cf34bb2ef5b6de3b101bedf45
Tor Anders Gustavsen <tor.anders@arkitektum.no>
2025-11-24 12:45:53 +0100

    Added kommuneplanforslag

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -3,5 +3,6 @@
 cd $GEOSERVER_DATA_DIR
 
 find . -name "*.sld" -exec sh -c 'envsubst < "$1" > "$1.tmp" && mv "$1.tmp" "$1"' _ {} \;
+find . -name "datastore.xml" -exec sh -c 'envsubst < "$1" > "$1.tmp" && mv "$1.tmp" "$1"' _ {} \;
 
 bash /opt/startup.sh
